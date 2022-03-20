6c2e48fce4f6e37152e299e1e230e231ab5a34e6
RisingOrange <jakub.fidler@protonmail.com>
2022-03-20 13:13:49 +0100

    Fix button width in anki >= 2.1.50

--- __init__.py
+++ __init__.py
@@ -21,43 +21,41 @@ def add_css_to_model(cmd: str, editor: Editor):
 
     def _compose_css(name: str, css: dict) -> str:
         if isinstance(css, dict):
-            css = ';\n'.join([*[f"{key}: {value}" for key, value in
-                css.items()], ""])[:-1]
+            css = ";\n".join([*[f"{key}: {value}" for key, value in css.items()], ""])[
+                :-1
+            ]
         if isinstance(css, list):
-            css = '\n'.join(css)
-        css = '\n  '.join(["", *css.split('\n')])[1:]
-        return (f".{name} {{\n"
-                f"{css}\n"
-                f"}}\n")
+            css = "\n".join(css)
+        css = "\n  ".join(["", *css.split("\n")])[1:]
+        return f".{name} {{\n" f"{css}\n" f"}}\n"
 
-    cssToAdd = '\n'.join(_compose_css(name, css) for name, css in config.items())
+    cssToAdd = "\n".join(_compose_css(name, css) for name, css in config.items())
 
     prefix = fr"BEGIN WRAPPER CODE {cmd}"
     prefix_comment = "Please do not edit this section directly, this code is generated automatically. Modify config.json instead"
     suffix = r"END WRAPPER CODE"
 
-    wrapper_code_new = (f"/* {prefix}\n"
-                       f"{prefix_comment} */\n"
-                       f"{cssToAdd}\n"
-                       f"/* {suffix} */")
+    wrapper_code_new = (
+        f"/* {prefix}\n" f"{prefix_comment} */\n" f"{cssToAdd}\n" f"/* {suffix} */"
+    )
 
-    wrapper_code_query = (f"/\* {prefix}.*\*/\n"
-                         f"(?P<style>.*?)\n"
-                         f"/\* {suffix} \*/")
+    wrapper_code_query = f"/\* {prefix}.*\*/\n" f"(?P<style>.*?)\n" f"/\* {suffix} \*/"
 
     model = editor.note.model()
-    css = model['css']
+    css = model["css"]
+
     def _save_css():
         model["css"] = css
         mw.col.models.save(model, updateReqs=False)
 
-    match = re.search(wrapper_code_query, css, flags=re.S|re.M)
+    match = re.search(wrapper_code_query, css, flags=re.S | re.M)
     if match is None:
         css += "\n" + wrapper_code_new
         _save_css()
-    elif match.group("style") != cssToAdd :
-        css = re.sub(wrapper_code_query, wrapper_code_new, css, flags=re.S|re.M)
+    elif match.group("style") != cssToAdd:
+        css = re.sub(wrapper_code_query, wrapper_code_new, css, flags=re.S | re.M)
         _save_css()
+
 
 def param_wraps(cmd: str) -> Callable[[Editor], None]:
     def result(editor: Editor):
@@ -66,26 +64,29 @@ def param_wraps(cmd: str) -> Callable[[Editor], None]:
 
         # Do the requested action
         c = getUserOption(["buttons", cmd])
-        [begin, end] = [c[i].replace("\\", "\\\\") for i in ['beginWrap', 'endWrap']]
+        [begin, end] = [c[i].replace("\\", "\\\\") for i in ["beginWrap", "endWrap"]]
         editor.web.eval(fr"callCmd({c['action']}, '{begin}', '{end}');")
 
     return result
+
 
 def init(rightoptbuttons: List[str], editor: Editor):
     def _parse_style(config) -> Dict:
         result = dict(**config)
 
         if not config.get("icon"):
-            result['icon'] = None
+            result["icon"] = None
         else:
-            result['icon'] = os.path.join(user_files_path, f"icons/{result['icon']}")
+            result["icon"] = os.path.join(user_files_path, f"icons/{result['icon']}")
 
         return result
 
     def add_btn(config) -> Callable:
         def _addButtonInvisible(keys, func, **kwargs):
             QShortcut(  # type: ignore
-                QKeySequence(keys), editor.widget, activated=lambda s=editor: func(s),
+                QKeySequence(keys),
+                editor.widget,
+                activated=lambda s=editor: func(s),
             )
 
         if config.get("visible", True):
@@ -93,18 +94,24 @@ def init(rightoptbuttons: List[str], editor: Editor):
         else:
             return _addButtonInvisible
 
-    buttons = [add_btn(config)(
-        cmd=cmd,
-        func=param_wraps(cmd),
-        **_parse_style(config.get("style", {})),
-    ) for cmd, config in getUserOption("buttons").items()]
+    buttons = [
+        add_btn(config)(
+            cmd=cmd,
+            func=param_wraps(cmd),
+            **_parse_style(config.get("style", {})),
+            id=f"meta-wrapper-btn-{cmd}"
+        )
+        for cmd, config in getUserOption("buttons").items()
+    ]
     rightoptbuttons.extend([i for i in buttons if i])
+
 
 def on_webview_will_set_content(web_content: WebContent, editor):
     if isinstance(editor, Editor):
         web_content.js.append(f"/_addons/{addon_package}/lib.js")
         web_content.js.append(f"/_addons/{addon_package}/commands.js")
         web_content.js.append(f"/_addons/{addon_package}/user_files/js.js")
+        web_content.css.append(f"/_addons/{addon_package}/btn.css")
 
 
 mw.addonManager.setWebExports(__name__, r"user_files/icons/.*")
