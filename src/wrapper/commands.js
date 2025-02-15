f6cbd98f4635fb347c316b6fbb3f9f007330cd47
Abdo <abdo@abdnh.net>
2025-02-15 16:12:12 +0300

    Add custom wrap() function

--- src/wrapper/commands.js
+++ src/wrapper/commands.js
@@ -4,6 +4,80 @@
  * }
  */
 
+
+// Adapted from https://github.com/ankitects/anki/pull/3038 to work around issue when wrapping lists
+// TODO: simplify logic
+function wrap2(begin, end) {
+  const {node: base} = this;
+  const selection = getFieldInputSelection();
+  const range = selection.getRangeAt(0);
+  if (!range) {
+      return;
+  }
+
+  let startParent = range.startContainer.parentNode;
+  if (
+      startParent !== base
+      && startParent.tagName !== "ANKI-EDITABLE"
+      && startParent?.firstChild === range.startContainer && range.startOffset === 0
+  ) {
+      range.setStartBefore(startParent);
+  }
+
+  let endParent = range.endContainer.parentNode;
+  if (
+      endParent !== base
+      && endParent.tagName !== "ANKI-EDITABLE"
+      && endParent?.lastChild === range.endContainer && (
+          (range.endContainer.nodeType !== Node.ELEMENT_NODE
+              && range.endOffset === range.endContainer.textContent?.length)
+          || (range.endContainer.nodeType === Node.ELEMENT_NODE
+              && range.endOffset === range.endContainer.childNodes.length)
+      )
+  ) {
+      range.setEndAfter(endParent);
+  }
+  let expand;
+  do {
+      expand = false;
+      if (
+          startParent
+          && startParent.parentNode !== base && startParent.parentNode.tagName !== "ANKI-EDITABLE" && startParent.parentNode?.firstChild === startParent
+          && range.isPointInRange(startParent.parentNode, startParent.parentNode?.childNodes.length)
+      ) {
+          startParent = startParent.parentNode;
+          range.setStartBefore(startParent);
+          expand = true;
+      }
+      if (
+          endParent && endParent.parentNode !== base && endParent.parentNode.tagName !== "ANKI-EDITABLE" && endParent.parentNode?.lastChild === endParent
+          && range.isPointInRange(endParent.parentNode, 0)
+      ) {
+          endParent = endParent.parentNode;
+          range.setEndAfter(endParent);
+          expand = true;
+      }
+      if (range.endOffset === 0 && range.endContainer.tagName !== "ANKI-EDITABLE") {
+          range.setEndBefore(range.endContainer);
+          expand = true;
+      }
+  } while (expand);
+
+  const fragment = range.extractContents();
+  if (fragment.childNodes.length === 0) {
+      document.execCommand("inserthtml", false, begin + end);
+  } else {
+      const div = document.createElement("div");
+      for(const node of Array.from(fragment.childNodes)) {
+        div.appendChild(node);
+      }
+      div.innerHTML = begin + div.innerHTML + end;
+      for(const node of div.childNodes) {
+        range.insertNode(node);
+      }
+  }
+}
+
 /* Move to end of pattern */
 function moveEnd() {
   const {endIndex} = this;
